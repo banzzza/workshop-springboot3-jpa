5c4aa01c181b40937a79478eeb0a87f1e92264b7
Bernardo Zardo Mergen <bernardozardomergen@gmail.com>
2022-11-04 10:40:15 -0300

    User Insert

--- src/main/java/com/cursosystem/course/entities/User.java
+++ src/main/java/com/cursosystem/course/entities/User.java
@@ -26,7 +26,6 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
-	
 	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
